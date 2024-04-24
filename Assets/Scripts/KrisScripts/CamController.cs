c02cc5e19eb0a361db1a750552f3319d9253ac68
Kristoffer Troelsen <kristroelsen@hotmail.com>
2024-04-24 20:05:27 +0200

    www

--- Assets/Scripts/KrisScripts/CamController.cs
+++ Assets/Scripts/KrisScripts/CamController.cs
@@ -23,38 +23,26 @@ public class CamController : MonoBehaviour
     {
         CamControl();
     }
-
-    /*void CamControl()
-    {
-        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
-        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
-        mouseY = Mathf.Clamp(mouseY, -35, 60);
-
-        Quaternion rootRotation = Quaternion.Euler(mouseY, mouseX, 0);
-
-        root.rotation = rootRotation;
-
-        hipJoint.targetRotation = Quaternion.Euler(0, -mouseX, 0);
-        stomachJoint.targetRotation = Quaternion.Euler(-mouseY + stomachOffset, 0, 0);
-    }*/
+    
 
     void CamControl()
+{
+    mouseX += Input.GetAxis("Mouse X") * rotationSpeed; // Horizontal movement controls y-axis rotation
+    mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed; // Vertical movement controls x-axis rotation
+    mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+    Quaternion rootRotation = Quaternion.Euler(mouseY, mouseX, 0);
+
+    if (Input.GetKey(KeyCode.Z))
     {
-        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
-        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
-        mouseY = Mathf.Clamp(mouseY, -35, 60);
-
-        Quaternion rootRotation = Quaternion.Euler(mouseY,mouseX, 0);
-
-        if (Input.GetKey(KeyCode.Z))
-        {
-            root.rotation = rootRotation;
-        }
-        else
-        {
-            root.rotation = rootRotation;
-            hipJoint.targetRotation = Quaternion.Euler(0, -mouseX, 0);
-            stomachJoint.targetRotation = Quaternion.Euler(-mouseY + stomachOffset, 0, 0);
-        }
+        root.rotation = rootRotation;
     }
+    else
+    {
+        root.rotation = rootRotation;
+        hipJoint.targetRotation = Quaternion.Euler(0, -mouseX, 0);
+        stomachJoint.targetRotation = Quaternion.Euler(-mouseY + stomachOffset, 0, 0);
+    }
+}
+
 }
---
+++ Assets/Scripts/KrisScripts/Grab.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour
+{
+    public Animator animator;
+    GameObject grabbedObj;
+    public Rigidbody rb;
+    public int isLeftorRight;
+    public bool alreadyGrabbing = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb= GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(isLeftorRight))
+        {
+            if(isLeftorRight == 0)
+            {
+                animator.SetBool("leftHandUp", true);
+            } else if(isLeftorRight == 1)
+            {
+                animator.SetBool("rightHandUp", true);
+            }
+
+            if(grabbedObj != null && !grabbedObj.GetComponent<FixedJoint>())
+        {
+            FixedJoint fj = grabbedObj.AddComponent<FixedJoint>();
+            fj.connectedBody = rb;
+            fj.breakForce = 9000;
+        } 
+
+        } else if(Input.GetMouseButtonUp(isLeftorRight))
+        {
+            if(isLeftorRight == 0)
+            {
+                animator.SetBool("leftHandUp", false);
+            } else if(isLeftorRight == 1)
+            {
+                animator.SetBool("rightHandUp", false);
+            }
+
+            if(grabbedObj != null)
+            {
+                Destroy(grabbedObj.GetComponent<FixedJoint>());
+            }
+
+            grabbedObj = null;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Item"))
+        {
+            grabbedObj = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        grabbedObj = null;
+    }
+
+}
