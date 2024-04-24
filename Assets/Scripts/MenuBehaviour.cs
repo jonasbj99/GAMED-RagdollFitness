f53866d78f2aace8334189eed5a85b98cf6d41a2
jonasbj99 <99955044+jonasbj99@users.noreply.github.com>
2024-04-24 20:18:22 +0200

    anim

---
+++ Assets/Scripts/MenuBehaviour.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBehavior : MonoBehaviour
+{
+    [SerializeField] Animator playMove;
+    [SerializeField] Animator settingsMove;
+    [SerializeField] Animator quitMove;
+    [SerializeField] Animator playWeight;
+    [SerializeField] Animator settingsWeight;
+    [SerializeField] Animator quitWeight;
+
+    //IEnumerator AnimateUI()
+    //{
+
+    //}
+}
