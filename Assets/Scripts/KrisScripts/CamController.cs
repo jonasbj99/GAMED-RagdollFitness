27d29ced0794e41f99499d38ef57c0294d4cc0e4
Kristoffer Troelsen <kristroelsen@hotmail.com>
2024-04-24 19:12:43 +0200

    a

--- Assets/Scripts/KrisScripts/CamController.cs
+++ Assets/Scripts/KrisScripts/CamController.cs
@@ -21,10 +21,10 @@ public class CamController : MonoBehaviour
 
     void FixedUpdate()
     {
-
+        CamControl();
     }
 
-    void CamControl()
+    /*void CamControl()
     {
         mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
         mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
@@ -36,5 +36,25 @@ public class CamController : MonoBehaviour
 
         hipJoint.targetRotation = Quaternion.Euler(0, -mouseX, 0);
         stomachJoint.targetRotation = Quaternion.Euler(-mouseY + stomachOffset, 0, 0);
+    }*/
+
+    void CamControl()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        Quaternion rootRotation = Quaternion.Euler(mouseY,mouseX, 0);
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            root.rotation = rootRotation;
+        }
+        else
+        {
+            root.rotation = rootRotation;
+            hipJoint.targetRotation = Quaternion.Euler(0, -mouseX, 0);
+            stomachJoint.targetRotation = Quaternion.Euler(-mouseY + stomachOffset, 0, 0);
+        }
     }
 }
