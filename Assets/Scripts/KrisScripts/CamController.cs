317b2f9e4de2a650fbd46a3c5ca3d621e00f53d4
Kristoffer Troelsen <kristroelsen@hotmail.com>
2024-04-25 12:39:38 +0200

    q

--- Assets/Scripts/KrisScripts/CamController.cs
+++ Assets/Scripts/KrisScripts/CamController.cs
@@ -34,32 +34,32 @@ public class CamController : MonoBehaviour
 
 
     void CamControl()
-{
-    mouseX += Input.GetAxis("Mouse X") * rotationSpeed; // Horizontal movement controls y-axis rotation
-    mouseY += Input.GetAxis("Mouse Y") * rotationSpeed; // Vertical movement controls x-axis rotation
-    mouseY = Mathf.Clamp(mouseY, -35, 60);
-
-    Quaternion rootRotation = Quaternion.Euler(0, mouseX, mouseY);
-
-    if (Input.GetKey(KeyCode.Z))
     {
-        mouseX += Input.GetAxis("Mouse X") * rotationSpeed; // Horizontal movement controls y-axis rotation
-        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed; // Vertical movement controls x-axis rotation
+        /*mouseX += Input.GetAxis("Mouse X") * rotationSpeed; // Horizontal movement controls y-axis rotation
+        mouseY += Input.GetAxis("Mouse Y") * rotationSpeed; // Vertical movement controls x-axis rotation
         mouseY = Mathf.Clamp(mouseY, -35, 60);
 
-        Quaternion rootRotation = Quaternion.Euler(0, mouseX, mouseY);
+        Quaternion rootRotation = Quaternion.Euler(0, mouseX, mouseY);*/
 
-        root.rotation = rootRotation;
-        
-        root.rotation = rootRotation;
-        hipJoint.targetRotation = Quaternion.Euler(0, -mouseX, 0);
-
-        /*if(Grab.alreadyGrabbing == false)
+        if (Input.GetKey(KeyCode.Z))
         {
-            stomachJoint.targetRotation = Quaternion.Euler(0, 0, -mouseY + stomachOffset);
-        }*/    
-    }
+            mouseX += Input.GetAxis("Mouse X") * rotationSpeed; // Horizontal movement controls y-axis rotation
+            mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed; // Vertical movement controls x-axis rotation
+            mouseY = Mathf.Clamp(mouseY, -35, 60);
 
-}
+            Quaternion rootRotation = Quaternion.Euler(0, mouseX, mouseY);
+
+            root.rotation = rootRotation;
+        
+            root.rotation = rootRotation;
+            hipJoint.targetRotation = Quaternion.Euler(0, -mouseX, 0);
+
+            /*if(Grab.alreadyGrabbing == false)
+            {
+            stomachJoint.targetRotation = Quaternion.Euler(0, 0, -mouseY + stomachOffset);
+            }*/    
+        }
+
+    }
 }
 
